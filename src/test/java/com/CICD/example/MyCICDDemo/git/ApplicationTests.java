408534be52d232017e25eb2a06397420191ba040
vponnap <getvenkathere@gmail.com>
2020-04-17 17:12:39 +0530

    this is my first repo

---
+++ src/test/java/com/CICD/example/MyCICDDemo/git/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CICD.example.MyCICDDemo.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
