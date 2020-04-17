41a8c75d56eb23a3bf1ce9184e1643eee142b4b7
vponnap <getvenkathere@gmail.com>
2020-04-17 17:15:19 +0530

    This is my second commit

---
+++ src/main/java/com/CICD/example/MyCICDDemo/git/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.CICD.example.MyCICDDemo.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/hello")
+    public String getMessage(){
+        return "hello Docker";
+    }
+}
